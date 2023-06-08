bb67af26e9f6b2826533286c7df55c3e73870d35
Zhanhao Zhang <zhangz73@cs.washington.edu>
2023-06-08 17:44:44 +0000

    add fine tuned results for power 1.5 different gammas

--- Stage2/gan2.py
+++ Stage2/gan2.py
@@ -362,7 +362,8 @@ class DynamicFactory():
             delta_phi_stn[:,t+1,-1] = phi_stn[:,t+1,-1] - phi_bar_stn[:,t+1,-1]
             phi_dot_stn[:,t,:] = (phi_stn[:,t+1,:] - phi_stn[:,t,:]) / DT
             
-            sigma_st[:,t] = ALPHA + (GAMMA_1 - GAMMA_2) / (GAMMA_1 + GAMMA_2) * (LAM / (2 ** (power - 1) * power)) ** (2 / (power + 2)) * ((GAMMA_1 + GAMMA_2) / 2 * ALPHA ** 2) ** (power / (power + 2)) * (ALPHA / XI_LIST[0]) ** ((4 - 2 * power) / (power + 2)) * g_tilda_prime_0 * XI_LIST[0] / ALPHA
+            sigma_st[:,t] = ALPHA + -1.153 * (GAMMA_1 - GAMMA_2) / (GAMMA_1 + GAMMA_2) * (LAM / (1 ** (power - 1) * power)) ** (2 / (power + 2)) * ((GAMMA_1 + GAMMA_2) / 2 * ALPHA ** 2) ** (power / (power + 2)) * (ALPHA / XI_LIST[0]) ** ((4 - 2 * power) / (power + 2)) * XI_LIST[0] / ALPHA
+            #sigma_st[:,t] = ALPHA + (GAMMA_1 - GAMMA_2) / (GAMMA_1 + GAMMA_2) * (LAM / (2 ** (power - 1) * power)) ** (2 / (power + 2)) * ((GAMMA_1 + GAMMA_2) / 2 * ALPHA ** 2) ** (power / (power + 2)) * (ALPHA / XI_LIST[0]) ** ((4 - 2 * power) / (power + 2)) * g_tilda_prime_0 * XI_LIST[0] / ALPHA
             mu_st[:,t] = GAMMA_BAR * S * sigma_st[:,t] ** 2 + 1/2 * (GAMMA_1 - GAMMA_2) * sigma_st[:,t] ** 2 * delta_phi_stn[:,t,0] + 1/2 * XI_LIST[0] * sigma_st[:,t] / ALPHA * (GAMMA_1 - GAMMA_2) * (ALPHA - sigma_st[:,t]) * self.W_st[:,t+1]
             stock_st[:,t+1] = stock_st[:,t] + mu_st[:,t] * DT + sigma_st[:,t] * self.dW_st[:,t]
         target = BETA * TR + ALPHA * self.W_st[:,-1]
@@ -689,11 +690,11 @@ train_args = {
     "dis_hidden_lst": [50, 50, 50],
     "combo_hidden_lst": [50, 50, 50],
     "gen_lr": [1e-2, 1e-2, 1e-2, 1e-2],
-    "gen_epoch": [500, 1000, 1000, 5000],#[500, 1000, 10000, 50000],
+    "gen_epoch": [10000, 1000, 500, 1000, 1000, 1000],#[500, 1000, 10000, 50000],
     "gen_decay": 0.1,
     "gen_scheduler_step": 100000,
-    "dis_lr": [1e-2, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-2, 1e-3],#[1e-2, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1],
-    "dis_epoch": [500, 1000, 1000, 5000],#[500, 2000, 10000, 50000],
+    "dis_lr": [1e-2, 1e-1, 1e-1, 1e-2],#[1e-2, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1],
+    "dis_epoch": [10000, 1000, 500, 1000, 1000, 1000],#[500, 2000, 10000, 50000],
     "dis_loss": [1],
     "utility_power": 1.5,
     "dis_decay": 0.1,
@@ -702,13 +703,13 @@ train_args = {
     "combo_epoch": [100000],#[500, 1000, 10000, 50000],
     "combo_decay": 0.1,
     "combo_scheduler_step": 50000,
-    "gen_sample": [128],
-    "dis_sample": [128],
+    "gen_sample": [3000, 1000],
+    "dis_sample": [3000, 1000],
     "combo_sample": [128, 128],
     "gen_solver": ["Adam"],
     "dis_solver": ["Adam"],
     "combo_solver": ["Adam"],
-    "total_rounds": 10,
+    "total_rounds": 1,#10,
     "normalize_up_to": 0,
     "visualize_obs": 0,
     "train_gen": True,
